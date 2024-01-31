5808baf1c07ce72c8754f5a59ab330e2a3fbe796
KangJeonggu <jeonggu9934@gmail.com>
2024-01-31 15:28:47 +0900

    계산기 두번째 빼기 추가

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ num1, num2, res = 100, 200, 0
 ##메인
 res = add_func(num1, num2)
 print(num1, '+', num2, '=', res)
+
+
+## 함수
+def sub_func(n1, n2):
+    return n1 - n2
+
+
+## 변수
+num1, num2, res = 100, 200, 0
+
+
+##메인
+res = sub_func(num1, num2)
+print(num1, '-', num2, '=', res)
