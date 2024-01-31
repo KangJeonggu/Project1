6b7aa24d6a52a323501bc72e591203be60ce9b17
KangJeonggu <jeonggu9934@gmail.com>
2024-01-31 15:31:28 +0900

    세번째 커밋 곱하기

--- main.py
+++ main.py
@@ -24,3 +24,17 @@ num1, num2, res = 100, 200, 0
 ##메인
 res = sub_func(num1, num2)
 print(num1, '-', num2, '=', res)
+
+
+## 함수
+def mul_func(n1, n2):
+    return n1 + n2
+
+
+## 변수
+num1, num2, res = 100, 200, 0
+
+
+##메인
+res = mul_func(num1, num2)
+print(num1, '*', num2, '=', res)
