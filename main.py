211fe553edf9ff9e9f97f804535c588d404665e1
KangJeonggu <jeonggu9934@gmail.com>
2024-01-31 15:35:59 +0900

    다섯번째 커밋 계산기 깔끔하게 정리

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 def add_func(n1, n2):
     return n1 + n2
 
+def sub_func(n1, n2):
+    return n1 - n2
+
+def mul_func(n1, n2):
+    return n1 * n2
+
+def div_func(n1, n2):
+    return n1 / n2
+
 
 ## 변수
 num1, num2, res = 100, 200, 0
@@ -11,37 +20,11 @@ num1, num2, res = 100, 200, 0
 res = add_func(num1, num2)
 print(num1, '+', num2, '=', res)
 
-
-## 함수
-def sub_func(n1, n2):
-    return n1 - n2
-
-
-## 변수
-num1, num2, res = 100, 200, 0
-
-
-##메인
 res = sub_func(num1, num2)
 print(num1, '-', num2, '=', res)
 
-
-## 함수
-def mul_func(n1, n2):
-    return n1 * n2
-
-
-## 변수
-num1, num2, res = 100, 200, 0
-
-
-##메인
 res = mul_func(num1, num2)
 print(num1, '*', num2, '=', res)
-
-
-def div_func(n1, n2):
-    return n1 / n2
 
 res = div_func(num1, num2)
 print(num1, '/', num2, '=', res)
